4ddb606a6494e27b2c90725b45fc5e630d450786
Francisco Rossi <francisco.rossi@lightit.io>
2023-03-16 14:31:33 -0300

    Translate the commands of helloWorld to a console command class in a separate file

--- helloWorld.php
+++ helloWorld.php
@@ -1,23 +1,16 @@
 #! /usr/bin/env php
 <?php
 
+use src\SayHelloCommand;
 use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Output\OutputInterface;
 
 require 'vendor/autoload.php';
 
 $app = new Application('Hello World Demo', '1.0');
 
 
-$app->register('sayHelloTo')
-    ->setDescription('Offer a greeting to the given person')
-    -> addArgument('name', InputArgument::OPTIONAL, 'Your name')
-    ->setCode(function (InputInterface $input, OutputInterface $output)
-    {
-        $name = $input ->getArgument('name');
-        $output->writeln("<info>Hello, $name<info>");
-    });
+$app->add(new SayHelloCommand);
+//add multiple commands.
+//example show scarface
 
 $app->run();
